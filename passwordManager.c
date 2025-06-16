a96dd2cce0279fa66f4328ecb82e7f78dfbce81c
xaizko <tonytran177013@gmail.com>
2025-06-15 22:16:55 -0400

    Added gtk's sample program to test

--- passwordManager.c
+++ passwordManager.c
@@ -1,6 +1,15 @@
 #include "passwordManager.h"
+#include "gui.h"
 
-int main() {
+int main (int argc,char **argv) {
+    int status;
+    GtkApplication *app;
 
-    return 0;
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
 }
+
