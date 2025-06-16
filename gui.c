84ae94fcec65b6f4c7760a322f9a4ca8e1a3ef1e
xaizko <tonytran177013@gmail.com>
2025-06-15 21:38:56 -0400

    Moving to a gui application

---
+++ gui.c
@@ -0,0 +1,10 @@
+void printMenu() {
+    printf("1. Add Password\n");
+    printf("2. Search for a service\n");
+    printf("3. Generate Password\n");
+    printf("4. Delete Password\n");
+    printf("5. List All Passwords\n");
+    printf("6. Exit\n");
+    
+    return;
+}
