a013d2819e022c675ca7b1c021ee5ad3a21188b6
xaizko <tonytran177013@gmail.com>
2025-06-15 22:17:19 -0400

    Added gtk's demo helloworld program and function declarations

--- gui.c
+++ gui.c
@@ -1,10 +1,20 @@
-void printMenu() {
-    printf("1. Add Password\n");
-    printf("2. Search for a service\n");
-    printf("3. Generate Password\n");
-    printf("4. Delete Password\n");
-    printf("5. List All Passwords\n");
-    printf("6. Exit\n");
-    
-    return;
+#include "gui.h"
+
+void print_hello (GtkWidget *widget,gpointer data) {
+    g_print ("Hello World\n");
+}
+
+void activate (GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Hello");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+    button = gtk_button_new_with_label ("Hello World");
+    g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
+    gtk_window_set_child(GTK_WINDOW (window), button);
+
+    gtk_window_present(GTK_WINDOW (window));
 }
