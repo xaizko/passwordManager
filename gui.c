db0a27ea83ea1c9b7f4385f3581c2c4840c75b04
xaizko <tonytran177013@gmail.com>
2025-06-16 23:24:39 -0400

    Testing login textbox

--- gui.c
+++ gui.c
@@ -1,20 +1,20 @@
 #include "gui.h"
 
-void print_hello (GtkWidget *widget,gpointer data) {
-    g_print ("Hello World\n");
-}
-
 void activate (GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
-    GtkWidget *button;
+    GtkWidget *box, *label;
 
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Hello");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Test 2");
+    gtk_window_set_default_size(GTK_WINDOW(window), 250, 250);
+    gtk_window_present(GTK_WINDOW(window));
 
-    button = gtk_button_new_with_label ("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
-    gtk_window_set_child(GTK_WINDOW (window), button);
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_window_set_child(GTK_WINDOW(window), box);
 
-    gtk_window_present(GTK_WINDOW (window));
+    label = gtk_label_new("Login");
+    gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(label, GTK_ALIGN_START);
+    gtk_box_append(GTK_BOX(box), label);
+
 }
--- gui.h
+++ gui.h
@@ -1,4 +1,3 @@
 #include <gtk/gtk.h>
 
 void activate (GtkApplication *app, gpointer user_data);
-void print_hello (GtkWidget *widget,gpointer data);
