fde98e9a6e69ee27cf2da4d46839107c382c246f
xaizko <tonytran177013@gmail.com>
2025-06-17 00:05:21 -0400

    Added --help flag, need to add --init flag

--- passwordManager.c
+++ passwordManager.c
@@ -1,17 +1,24 @@
 #include "passwordManager.h"
 #include "gui.h"
+#include "cliFunctions.h"
 
 int main (int argc,char **argv) {
-    int status;
-    GtkApplication *app;
 
-    app = gtk_application_new ("com.github.xaizko.passwordManager", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+    if (argc > 1) {
+	if (!strcmp(argv[1], "--help")) {
+	    printHelp();
+	}
+    } 
 
-    printf("Hello world\n");
-    printf("This is a test\n");
-    return status;
+    //int status;
+    //GtkApplication *app;
+
+    //app = gtk_application_new ("com.github.xaizko.passwordManager", G_APPLICATION_DEFAULT_FLAGS);
+    //g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+    //status = g_application_run (G_APPLICATION (app), argc, argv);
+    //g_object_unref (app);
+
+    //return status;
+    return 0;
 }
 
