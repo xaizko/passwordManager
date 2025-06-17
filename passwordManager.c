3ef63f9dc7279ccdf8e86d2101105f81b5dcf575
xaizko <tonytran177013@gmail.com>
2025-06-16 23:33:53 -0400

    Minor tweaks

--- passwordManager.c
+++ passwordManager.c
@@ -5,11 +5,13 @@ int main (int argc,char **argv) {
     int status;
     GtkApplication *app;
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    app = gtk_application_new ("com.github.xaizko.passwordManager", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
 
+    printf("Hello world\n");
+    printf("This is a test\n");
     return status;
 }
 
